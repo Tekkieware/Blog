fb293ec7756ca023fc22ea8ef595c8b1c293b3c9
Isaiah Ozadhe <tekkieware@gmail.com>
2025-10-02 14:56:17 +0100

    get post update post delete post api route

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,44 @@
+
+import dbConnect from "@/lib/db";
+import Post from "@/models/post";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    await dbConnect();
+    try {
+        const post = await Post.findById(params.id);
+        if (!post) {
+            return NextResponse.json({ message: "Post not found" }, { status: 404 });
+        }
+        return NextResponse.json(post, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to fetch post" }, { status: 500 });
+    }
+}
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
+    await dbConnect();
+    try {
+        const postData = await request.json();
+        const updatedPost = await Post.findByIdAndUpdate(params.id, postData, { new: true });
+        if (!updatedPost) {
+            return NextResponse.json({ message: "Post not found" }, { status: 404 });
+        }
+        return NextResponse.json(updatedPost, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to update post" }, { status: 500 });
+    }
+}
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    await dbConnect();
+    try {
+        const deletedPost = await Post.findByIdAndDelete(params.id);
+        if (!deletedPost) {
+            return NextResponse.json({ message: "Post not found" }, { status: 404 });
+        }
+        return NextResponse.json({ message: "Post deleted successfully" }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "Failed to delete post" }, { status: 500 });
+    }
+}
