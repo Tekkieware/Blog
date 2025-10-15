ed5d44b1c00404cd8b5aa5131623755ac0172be6
Isaiah Ozadhe <tekkieware@gmail.com>
2025-10-15 12:45:30 +0100

    admin layout

---
+++ app/admin/layout.tsx
@@ -0,0 +1,11 @@
+
+import AdminNav from '@/components/admin-nav';
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <AdminNav />
+      {children}
+    </div>
+  );
+}
